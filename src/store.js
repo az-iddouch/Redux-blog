da4188f346f23f97bb6ef662d479269f1bc38d37
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-10-07 18:32:09 +0100

    refactored the store code so the app can work even if the user doesn't have redux dev tools

--- src/store.js
+++ src/store.js
@@ -4,13 +4,20 @@ import reduxPromiseMiddleware from 'redux-promise';
 
 // const initialState = {};
 const middleware = [reduxPromiseMiddleware];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
+
+// yooo
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
